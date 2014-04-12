93d5317923904ce4cd32c38deda179b0deae9e3f
Sp1rit <ganea.dinu@yahoo.com>
2014-04-12 18:09:14 +0300

    update

---
+++ database/cich.sql
@@ -0,0 +1,44 @@
+-- Adminer 4.0.3 MySQL dump
+
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = '+03:00';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `parent_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `menu` (`id`, `name`, `parent_id`) VALUES
+(4,	'Home',	0),
+(5,	'main',	4);
+
+DROP TABLE IF EXISTS `pages`;
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `text` longtext CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `pages` (`id`, `title`, `text`) VALUES
+(6,	'Test',	'<p>content</p>\r\n'),
+(7,	'page_title',	'<p>page_content</p>\r\n'),
+(8,	'page_35',	'<p>dkasl;kjflhd kja</p>\r\n');
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1,	'root',	'$2y$10$JXXB.EZfMEnNMq6KZm.vy.dC7OQoZfVOGsN9pfQYBvxOwUPiGRUi6');
+
+-- 2014-04-12 18:07:17
